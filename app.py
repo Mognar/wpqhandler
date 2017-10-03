988816b0d18050fcd0276b2c25d26deb0acc9886
Mognar <philipbgorman@gmail.com>
2017-10-03 14:45:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -409,7 +409,8 @@ def index():
        abtxt=''.join(['\n\t- {} for the {}'.format(b[1], b[0]) for b in h.itemCounts[0]])
        questionoutput = '----\n{} needs to tag {} questions:{}</br></br>'.format(h.name, h.itemCounts[1],abtxt)
        questions.append(questionoutput)
-return render_template('index.html', questionsoutput=questions)
+         
+   return render_template('index.html', questionsoutput=questions)
 
 # ## General Reports
 # 
