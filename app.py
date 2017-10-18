e9c42ca497827682dd30c822155a1254f65344cf
Mognar <philipbgorman@gmail.com>
2017-10-18 15:25:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ def index():
    questions=[]
    for h in handlerJobs:
        abtxt='<br />'.join(['{} for the {}'.format(b[1], b[0]) for b in h.itemCounts[0]])
-       questionoutput = '<b>{}</b> needs to tag {} questions:{}<br />'.format(h.name, h.itemCounts[1],abtxt)
+       questionoutput = '<br /><b>{}</b> needs to tag {} questions:<br />{}'.format(h.name, h.itemCounts[1],abtxt)
        questions.append(questionoutput)
          
    return render_template('index.html', questionsoutput=questions)
