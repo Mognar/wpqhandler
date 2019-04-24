a08376d234f9d470e91e054e396fd2fa9e64e85f
Mognar <philipbgorman@gmail.com>
2019-04-24 13:53:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def my_form_post():
         #print(questionsoutput)
         
     questions = handlerJobs    
-    dfq = pd.DataFrame([vars(s) for q in questions])
+    dfq = pd.DataFrame([vars(q) for q in questions])
     print(dfq)
          
     return render_template('resultpage.html', questions = questions, handling = handling)
